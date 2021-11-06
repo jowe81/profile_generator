9ef1ef019af742797a6f5d4e37171a3eed9e7c2f
Johannes Weber <johannes@drweber.de>
2021-11-06 16:19:47 -0700

    app works!

--- survey.js
+++ survey.js
@@ -1,7 +1,5 @@
 const readline = require('readline');
-
 const { stdin: input, stdout:output } = require('process');
-
 const rl = readline.createInterface({ input, output});
 
 const questions = [
@@ -14,7 +12,27 @@ const questions = [
   "What is your superpower? In a few words, tell us what you are amazing at!"
 ];
 
-rl.question("What's your name or nickname? ", answer => {
-  console.log(answer);
+let answers = [];
+let questionsAnswered = 0;
+
+const checkIfDone = () => questionsAnswered >= questions.length;
+
+const generateOutput = () => {
+  const output = answers[0] + " loves listening to "
+    + answers[2] + " while " + answers[1] + ", devours "
+    + answers[4] + " for " + answers[3] + ", prefers "
+    + answers[5] + " over every other sport, and is amazing at "
+    + answers[6] + ".";
+  console.log(output);
   rl.close();
-});
+};
+
+const askQuestion = (questionNo) => {
+  rl.question(questions[questionNo] + " ", answer => {
+    answers.push(answer);
+    checkIfDone(++questionsAnswered) ? generateOutput() : askQuestion(questionNo + 1);
+  });
+};
+
+askQuestion(0);
+
